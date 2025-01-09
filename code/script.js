9d9e16dab905898ed27f66dd42d62d101008ebc8
lungaho ramsey <lungahoramsey2005@gmail.com>
2025-01-09 20:48:34 +0300

    thu

---
+++ code/script.js
@@ -0,0 +1,17 @@
+// structure of a for loop
+for (iterator; condition; iteration) {
+    // Code to be executed in each loop iteration
+}
+//The .unshift() method of an array allows you to add a value to the beginning of the array
+
+//.push() which adds the value at the end of the array. .push() which adds the value at the end of the array.
+//.pop() removes the last element of an array
+//.shift() removes the first element of an array
+//.splice() method to remove elements from an array
+
+//if statement syntax
+if (condition) {
+    // Code to execute if the condition is true
+}
+//a shorthand notation for defining functions in JavaScript.
+//=>
